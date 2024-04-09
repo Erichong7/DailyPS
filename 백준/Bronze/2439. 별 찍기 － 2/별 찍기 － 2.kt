9bae7cd308367dd96010816aa5e0f3c923165a82
Erichong7 <97429550+Erichong7@users.noreply.github.com>
2024-04-10 03:33:32 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 272 ms, Memory: 14952 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val n = nextInt()
+
+    for (i in 1..n) {
+        for (j in n downTo 1) {
+            print(
+                if(i >= j) "*"
+                else " "
+            )
+        }
+        println()
+    }
+}
