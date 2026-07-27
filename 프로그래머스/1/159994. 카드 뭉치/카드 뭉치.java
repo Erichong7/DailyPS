9928bdddbbc45d270f8e28841c091eab2b0e6f77
Erichong7 <97429550+Erichong7@users.noreply.github.com>
2026-07-27 19:53:11 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -4,15 +4,14 @@ class Solution {
         int index1 = 0;
         int index2 = 0;
         for(String goal : goals) {
-            boolean match1 = index1 < cards1.length && cards1[index1].equals(goal);
-            boolean match2 = index2 < cards2.length && cards2[index2].equals(goal);
-
-            if (!match1 && !match2) {
+            if (index1 < cards1.length && cards1[index1].equals(goal)) {
+                index1++;
+            } else if (index2 < cards2.length && cards2[index2].equals(goal) ) {
+                index2++;
+            } else {
                 answer = "No";
-                break;
             }
-            if (match1) index1++;
-            if (match2) index2++;
+            
         }
         return answer;
     }
