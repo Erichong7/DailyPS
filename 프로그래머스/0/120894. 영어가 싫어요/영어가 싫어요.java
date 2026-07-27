d789b190d626bdd027951635f3f531a51d83a6cf
Erichong7 <97429550+Erichong7@users.noreply.github.com>
2026-07-28 01:15:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 6.24 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,52 @@
+class Solution {
+    public long solution(String numbers) {
+        String answer = "";
+        String temp = "";
+        for (int i = 0; i < numbers.length(); i++) {
+            temp += numbers.charAt(i);
+            switch (temp) {
+                case "zero":
+                    answer += "0";
+                    temp = "";
+                    break;
+                case "one":
+                    answer += "1";
+                    temp = "";
+                    break;
+                case "two":
+                    answer += "2";
+                    temp = "";
+                    break;
+                case "three":
+                    answer += "3";
+                    temp = "";
+                    break;
+                case "four":
+                    answer += "4";
+                    temp = "";
+                    break;
+                case "five":
+                    answer += "5";
+                    temp = "";
+                    break;
+                case "six":
+                    answer += "6";
+                    temp = "";
+                    break;
+                case "seven":
+                    answer += "7";
+                    temp = "";
+                    break;
+                case "eight":
+                    answer += "8";
+                    temp = "";
+                    break;
+                case "nine":
+                    answer += "9";
+                    temp = "";
+                    break;
+            }
+        }
+        return Long.parseLong(answer);
+    }
+}
