3db54a5efb710a30472427df1a9de1e24f1461ef
Erichong7 <97429550+Erichong7@users.noreply.github.com>
2026-07-29 01:28:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for(int i = 0; i < quiz.length; i++) {
+            String[] str = quiz[i].split(" ");
+            int num1 = Integer.parseInt(str[0]);
+            int num2 = Integer.parseInt(str[2]);
+            int result = Integer.parseInt(str[4]);
+            answer[i] = result == (str[1].equals("+") ? num1 + num2 : num1 - num2) ? "O" : "X";
+        }
+        return answer;
+    }
+}
