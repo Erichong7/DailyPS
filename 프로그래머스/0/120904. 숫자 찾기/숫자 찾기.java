f886aaf82e594194d80fbb35550c2d3c0ab938ee
Erichong7 <97429550+Erichong7@users.noreply.github.com>
2026-07-29 01:20:33 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        char[] arr = String.valueOf(num).toCharArray();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == k + '0') {
+                return i + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
